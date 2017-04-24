f71eabd7422c9f9dbf733df05e5e1dbdf8fd5feb
urskenner <urskenner@web.de>
2017-04-24 20:26:10 +0200

    Aufgabe 3a funktioniert

--- src/ex1/part3/MySerializableClass.java
+++ src/ex1/part3/MySerializableClass.java
@@ -2,21 +2,20 @@ package ex1.part3;
 
 import java.io.Serializable;
 
-public class MySerializableClass implements Serializable{
-	private static final long serialVersionUID=1;
-	private int id;
-	private String string;
-	
-	MySerializableClass() {
-		id=1234;
-	}
-	
-	public void set(String string) {
-		this.string=string;
-	}
-	
-	public String toString() {
-		return "id: "+id+"; string: "+string;
-	}
+public class MySerializableClass implements Serializable {
+    private static final long serialVersionUID = 1;
+    private int id;
+    private String string;
+
+    MySerializableClass() {
+        id = 1234;
+    }
+
+    public void set(String string) {
+        this.string = string;
+    }
+
+    public String toString() {
+        return "id: " + id + "; string: " + string;
+    }
 } 
-	
