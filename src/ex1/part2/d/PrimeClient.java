2d97362cbca95de61d4d2ab9297bc52276e4c5cb
urskenner <urskenner@web.de>
2017-05-03 00:06:42 +0200

    c in de integriert aufgabenteil d

--- src/ex1/part2/d/PrimeClient.java
+++ src/ex1/part2/d/PrimeClient.java
@@ -11,7 +11,7 @@ public class PrimeClient {
 	private static final String HOSTNAME="localhost";
 	private static final int PORT=1234;
 	private static final long INITIAL_VALUE=(long)1e17;
-	private static final String REQUESTMODE = "syncronized";
+	private static final String REQUESTMODE = "synchronized";
 	private static final long COUNT=20;
 	private static final String CLIENT_NAME= PrimeClient.class.getName();
 
@@ -20,7 +20,7 @@ public class PrimeClient {
 	int port;
 	String requestmode;
 	long initialValue,count;
-	
+
 	public PrimeClient(String hostname,int port,long initialValue, String requestMode,long count) {
 		this.hostname=hostname;
 		this.port=port;
@@ -36,32 +36,68 @@ public class PrimeClient {
 			//System.out.println("Process " + i);
 			processNumber(i);
 		}
-    }
-	    
-    public void processNumber(long value) throws IOException, ClassNotFoundException {
+	}
+
+	public void processNumber(long value) throws IOException, ClassNotFoundException {
 
 		communication.send(new Message(hostname, port, new Long(value)), true);
 		Message m = null;
 
+		class Threadi extends Thread {
+			private boolean ende = false;
+
+			public void run() {
+
+				while(!ende) {
+					System.out.print(".");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException ec) {
+						ec.printStackTrace();
+					}
+				}
+			}
+
+			public void setEnde(boolean ende) {
+				this.ende = ende;
+			}
+
+		}
 
 
-
-		if (requestmode.equals("syncronized")) {
-			System.out.println(value+":");
+		if (requestmode.equals("synchronized")) {
+			System.out.print(value+": ");
 			Boolean isPrime = (Boolean) communication.receive(port, true, true).getContent();
-			System.out.println(value + ": " + (isPrime.booleanValue() ? "prime" : "not prime"));
+			System.out.print((isPrime.booleanValue() ? "prime\n" : "not prime\n"));
 
 		} else if (requestmode.equals("polling")){
 
-			System.out.println(value + " :" + "... ");
+			System.out.print(value + " :" + "...");
+			Threadi x = new Threadi();
+			x.start(); //.... für die Ausgabe erzeugen
 
 			do {
 				m = communication.receive(port, false, true);
 			} while (m == null);
 
 			Boolean isPrime = (Boolean) m.getContent();
-			System.out.println(value + " :" + "........ " + (isPrime.booleanValue() ? "prime" : "not prime"));
+			x.setEnde(true);
+			System.out.print((isPrime.booleanValue() ? " prime\n" : " not prime\n"));
 
+		} else if(requestmode.equals("block2")) {
+
+			System.out.print(value + " :" + "...");
+			Threadi t = new Threadi();
+			t.start();
+			Boolean isPrime = (Boolean) communication.receive(port, true, true).getContent();
+			t.setEnde(true);
+			System.out.print((isPrime.booleanValue() ? " prime\n" : " not prime\n"));
+
+
+		} else {
+			System.out.println("Falsche Eingabe. Bitte folgende Eingabe tätigen: synchronized (blockierende-modus), polling (nicht-blockierend), block2 (blockierend + thread)");
+			System.out.println("Client neustarten!");
+			System.exit(-1);
 		}
 	}
 
@@ -72,20 +108,20 @@ public class PrimeClient {
 		int port=PORT;
 		long initialValue=INITIAL_VALUE;
 		String requestmode = REQUESTMODE;
-		long count=COUNT;		
-		
+		long count=COUNT;
+
 		boolean doExit=false;
-				
+
 		String input;
-		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in )); 
-		
+		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in ));
+
 		System.out.println("Welcome to "+CLIENT_NAME+"\n");
-		
+
 		while(!doExit) {
 			System.out.print("Server hostname ["+hostname+"] > ");
 			input=reader.readLine();
 			if(!input.equals("")) hostname=input;
-			
+
 			System.out.print("Server port ["+port+"] > ");
 			input=reader.readLine();
 			if(!input.equals("")) port=Integer.parseInt(input);
@@ -93,7 +129,7 @@ public class PrimeClient {
 			System.out.print("Request mode ["+requestmode+"] > ");
 			input=reader.readLine();
 			if(!input.equals("")) requestmode=input;
-			
+
 			System.out.print("Prime search initial value ["+initialValue+"] > ");
 			input=reader.readLine();
 			if(!input.equals("")) initialValue=Integer.parseInt(input);
@@ -101,13 +137,12 @@ public class PrimeClient {
 			System.out.print("Prime search count ["+count+"] > ");
 			input=reader.readLine();
 			if(!input.equals("")) count=Integer.parseInt(input);
-			
+
 			new PrimeClient(hostname,port,initialValue,requestmode,count).run();
-			
+
 			System.out.println("Exit [n]> ");
 			input=reader.readLine();
 			if(input.equals("y") || input.equals("j")) doExit=true;
 		}
 	}
 }
-	
